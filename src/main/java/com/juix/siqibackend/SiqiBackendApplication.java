c591f576fd8859a7da97681b4aa2c1ffc02a64f2
KingJ <501315555@qq.com>
2019-03-26 00:19:52 +0800

    Initial Submit
    Initial Submit

---
+++ src/main/java/com/juix/siqibackend/SiqiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.juix.siqibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiqiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiqiBackendApplication.class, args);
+    }
+
+}
